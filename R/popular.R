d4b05543d88ec5b0801ce43ac61d51b3f248be8d
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-24 00:48:14 -0400

    Create get_most_emailed(), add to readme

--- R/popular.R
+++ R/popular.R
@@ -40,3 +40,32 @@ get_most_viewed <- function(period = 30, key = NULL, parse = TRUE) {
   return(resp)
   }
 }
+
+#' Get most emailed articles
+#'
+#' `r lifecycle::badge('experimental')`
+#'
+#' @param period Numeric. Period to get data. Either 1, 7 or 30. Defaults to 30.
+#' @param key String. NYT key.
+#' @param parse Logical. TRUE to return parsed JSON, FALSE to return HTTP
+#' response.
+#'
+#' @return HTTP response or parsed JSON.
+#' @export
+get_most_emailed <- function(period = 30, key = NULL, parse = TRUE) {
+  path <- paste0("/emailed/", period, ".", "json")
+
+  params <- create_query_list("api-key" = key)
+
+  resp <- create_popular_req() |>
+    httr2::req_url_path_append(path) |>
+    httr2::req_url_query(!!!params) |>
+    httr2::req_perform()
+
+  if (parse) {
+    json <- resp |> httr2::resp_body_json()
+    return(json)
+  } else {
+    return(resp)
+  }
+}
