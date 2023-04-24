1d69b2dabeb39b0c6f26c9604922aa196f6f5780
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-23 20:31:12 -0400

    Fix test

--- tests/testthat/test-request.R
+++ tests/testthat/test-request.R
@@ -1,6 +1,6 @@
 test_that("create request works", {
   data <- create_req()
 
-  expect_equal(data[["url"]], "https://api.nytimes.com")
+  expect_equal(data[["url"]], "https://api.nytimes.com/svc")
   expect_s3_class(data, "httr2_request")
 })
