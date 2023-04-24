627d08fac808f500919b27f1dc3910afd679df61
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-23 20:30:50 -0400

    Add tests

--- tests/testthat/test-request.R
+++ tests/testthat/test-request.R
@@ -1,3 +1,6 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("create request works", {
+  data <- create_req()
+
+  expect_equal(data[["url"]], "https://api.nytimes.com")
+  expect_s3_class(data, "httr2_request")
 })
