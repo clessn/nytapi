533801be3f0fcc4ef7bc0bef31dfa89cf1d9cb59
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-23 23:12:17 -0400

    Test create_popular_req()

--- R/popular.R
+++ R/popular.R
@@ -1,29 +1,13 @@
-#' Get most popular articles
+#' Create most popular request
 #'
 #' `r lifecycle::badge('experimental')`
 #'
-#' @param query Search query
-#' @param filter Search filters
-#' @param ... Other things to pass onto query
-#' @param key API key
+#' @keywords internal
 #'
-#' @return HTTR response
+#' @return A modified HTTP request.
 #' @export
-search_articles <- function(query = NULL, filter = NULL, ..., key = NULL){
-
-  params <- list(
-    q = query,
-    fq = filter,
-    ...,
-    "api-key" = key
-  )
-
-  names(params) <- paste0(names(params))
-
+#'
+create_popular_req <- function() {
   create_req() |>
-    httr2::req_url_path_append("/search/v2/articlesearch.json") |>
-    httr2::req_url_query(!!!params) |>
-    nytapi::add_user_agent() |>
-    httr2::req_throttle(5/60) |>
-    httr2::req_perform()
+    httr2::req_url_path_append("/mostpopular/v2")
 }
