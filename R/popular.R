58ea78163dfe03ac37b4f236fa05c8e38eaf81c2
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-23 23:54:49 -0400

    Create `get_most_viewed()` (#28)
    * Create function
    
    * Document parameters
    
    * Add return value
    
    * Add parse parameter
    
    * Increment version number to 0.0.0.9008

--- R/popular.R
+++ R/popular.R
@@ -11,3 +11,27 @@ create_popular_req <- function() {
   create_req() |>
     httr2::req_url_path_append("/mostpopular/v2")
 }
+
+#' Get most viewed articles
+#'
+#' @param period Numeric. Period to get data. Either 1, 7 or 30. Defaults to 30.
+#' @param key String. NYT key.
+#' @param parse Logical. TRUE to return parsed JSON, FALSE to return HTTP
+#' response.
+#'
+#' @return HTTP response or parsed JSON.
+#' @export
+get_most_viewed <- function(period = 30, key = NULL, parse = TRUE) {
+  path <- paste0("/viewed/", period, ".", "json?api-key=", key)
+
+  resp <- create_popular_req() |>
+    httr2::req_url_path_append(path) |>
+    httr2::req_perform()
+
+  if (parse) {
+    json <- resp |> httr2::resp_body_json()
+    return(json)
+  } else {
+  return(resp)
+  }
+}
