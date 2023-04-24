7fd1f51b3b3996ab63bab46b367a990919e7addb
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-23 23:13:19 -0400

    Fix search_articles()

--- R/search.R
+++ R/search.R
@@ -1,13 +1,30 @@
-#' Create most popular request
+#' Look up articles by keywords
 #'
 #' `r lifecycle::badge('experimental')`
 #'
-#' @keywords internal
+#' @param query Search query
+#' @param filter Search filters
+#' @param ... Other things to pass onto query
+#' @param key API key
 #'
-#' @return A modified HTTP request.
+#' @return HTTR response
 #' @export
-#'
-create_popular_req <- function() {
+search_articles <- function(query = NULL, filter = NULL, ..., key = NULL){
+
+  params <- list(
+    q = query,
+    fq = filter,
+    ...,
+    "api-key" = key
+  )
+
+  names(params) <- paste0(names(params))
+
   create_req() |>
-    httr2::req_url_path_append("/mostpopular/v2")
+    httr2::req_url_path_append("/search/v2/articlesearch.json") |>
+    httr2::req_url_query(!!!params) |>
+    nytapi::add_user_agent() |>
+    httr2::req_throttle(5/60) |>
+    httr2::req_perform()
+
 }
