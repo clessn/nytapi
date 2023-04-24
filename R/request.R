fc861358a4d47e17063281955d56f1e594a04e12
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-23 20:35:28 -0400

    Write function

--- R/request.R
+++ R/request.R
@@ -11,3 +11,19 @@ create_req <- function() {
   httr2::request("https://api.nytimes.com") |>
     httr2::req_url_path_append("/svc")
 }
+
+#' Add user agent to request
+#'
+#' `r lifecycle::badge('experimental')`
+#'
+#' @param req A HTTP request.
+#'
+#' @keywords internal
+#'
+#' @return A modified HTTP request.
+#' @export
+#'
+add_user_agent <- function(req) {
+  req |>
+    httr2::req_user_agent("nytapi (https://github.com/clessn; info@clessn.ca)")
+}
